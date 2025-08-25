4a986304dee80ef639c4af67a45da31768f241ea
daluchiudechukwu <daluchizy@yahoo.com>
2025-08-25 14:11:45 +0100

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
-module "sandbox" {
+module "bluevarsity" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "dev@bluevarsity.com"
+    AccountName               = "blue-varsity"
+    ManagedOrganizationalUnit = "OU-ONE"
+    SSOUserEmail              = "dev@bluevarsity.com"
+    SSOUserFirstName          = "Blue"
+    SSOUserLastName           = "Varsity"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "OU-ONE" = "Blue Varsity"
   }
 
   change_management_parameters = {
@@ -23,5 +23,117 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "bluevarsity"
+}
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "dev@mybirthpals.com.ng"
+    AccountName               = "my-birthpals"
+    ManagedOrganizationalUnit = "OU-ONE"
+    SSOUserEmail              = "dev@mybirthpals.com.ng"
+    SSOUserFirstName          = "Mybirth"
+    SSOUserLastName           = "Pals"
+  }
+
+  account_tags = {
+    "OU-ONE" = "My Birth Pals"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "mybirthpals"
+}
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "dev@edenetravel.com"
+    AccountName               = "edene-travel"
+    ManagedOrganizationalUnit = "OU-ONE"
+    SSOUserEmail              = "dev@edenetravel.com"
+    SSOUserFirstName          = "Edene"
+    SSOUserLastName           = "Travel"
+  }
+
+  account_tags = {
+    "OU-ONE" = "Edene Travel"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "edenetravel"
+}
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "ict@fedpolynekfoundation.com"
+    AccountName               = "ict-fedpolyfdn"
+    ManagedOrganizationalUnit = "OU-ONE"
+    SSOUserEmail              = "ict@fedpolynekfoundation.com"
+    SSOUserFirstName          = "ICT"
+    SSOUserLastName           = "FedPolyNek"
+  }
+
+  account_tags = {
+    "OU-ONE" = "ICT FedPolyNek"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "ictfedpolynek"
+}
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "ceelatech7@gmail.com"
+    AccountName               = "Ceela-tech"
+    ManagedOrganizationalUnit = "OU-ONE"
+    SSOUserEmail              = "ceelatech7@gmail.com"
+    SSOUserFirstName          = "Ceela"
+    SSOUserLastName           = "Tech"
+  }
+
+  account_tags = {
+    "OU-ONE" = "Ceela Tech"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "ceelatech"
 }
